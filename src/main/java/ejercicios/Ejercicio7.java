9976418090253c99f4a2fce1840b46668d34010a
carlosig490@gmail.com <carlosig490@gmail.com>
2021-05-11 19:51:40 +0200

    aaaaaaah

---
+++ src/main/java/ejercicios/Ejercicio7.java
@@ -0,0 +1,15 @@
+package ejercicios;
+
+/**
+ * 8.7. Copia las clases anteriores como una nueva versión de las mismas (añade
+ * sufijo _v7) y realiza los siguientes cambios: • Figura2D_v7 tendrá un nuevo
+ * constructor, que recibe como parámetro otro objeto de tipo Figura2D_v7 y que
+ * copiará todos sus datos. • Hacer otro constructor análogo para la clase
+ * Triangulo_v7. • Hay un programa que cree un Triangulo_v7 con el constructor
+ * de 3 parámetros creado previamente, y otro triángulo utilizando el nuevo
+ * constructor. Mostrar el área de ambos.
+ */
+
+public class Ejercicio7 {
+
+}

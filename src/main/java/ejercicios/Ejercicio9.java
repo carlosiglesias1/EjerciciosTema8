aa475b58f554c39fd4263f59eada643cf89a8f01
carlosig490@gmail.com <carlosig490@gmail.com>
2021-05-12 12:32:25 +0200

    bla bla

---
+++ src/main/java/ejercicios/Ejercicio9.java
@@ -0,0 +1,19 @@
+package ejercicios;
+
+/**
+ * 8.9. Copia la clase _CuentaCorriente que te proporcionará el profesor con un
+ * nombre nuevo, por ejemplo, CuentaCorriente y realiza las siguientes
+ * operaciones: • Crea una clase hija llamada CuentaPlazo igual que la anterior,
+ * pero sin comisiones. Además, tiene un nuevo atributo, que es la fecha de
+ * creación. • Esta nueva clase ya no permitirá que haya subclases de ella. •
+ * Crea constructor para la nueva clase que incorpore la inicialización de la
+ * fecha de creación al día en curso. • Sobrescribe el método retirar () para
+ * que no que no calcule comisiones. Revisa los modificadores de acceso de
+ * atributos y quizás tengas que crear algún getter/setter. • Haz un programa
+ * que cree inicialmente una cuenta a plazo y luego, mediante un menú, permita
+ * ingresar, retirar y consultar el saldo.
+ */
+
+public class Ejercicio9 {
+
+}
